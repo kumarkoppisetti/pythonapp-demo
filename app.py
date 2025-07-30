8c2c3c4669644405439c1653cf066fcabfb6c1f6
kumarkoppisetti <98539302+kumarkoppisetti@users.noreply.github.com>
2025-07-30 07:33:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Flask Docker CI/CD!"
+    return "Docker image successfully deployed in EC2-server"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
